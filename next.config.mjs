786550ed5f8d5e17efa07804a67ebbd5e916e5bd
LeeSangWon <swlee@bizflow.com>
2024-03-16 12:00:26 +0900

    commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+// const withBundleAnalyzer = import('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
